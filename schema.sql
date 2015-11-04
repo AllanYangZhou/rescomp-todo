a49638559188cadb8ed6f61200af13a0439f1be1
samyhajal <hajalsamy@gmail.com>
2015-11-03 19:20:07 -0800

    updated status default

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS todos;
 CREATE TABLE todos (
 	"id" integer PRIMARY KEY AUTOINCREMENT,
 	"description" varchar(140),
-	"status" boolean,
+	"status" boolean DEFAULT 0,
 	"created_date" timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
