3e769ff597fefa3ddfb351d0a9280e31f18d6edb
Zizheng Tai <zizheng.tai@gmail.com>
2015-11-03 19:05:04 -0800

    done things

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import sqlite3
-from flask import Flask, render_template, request
+from datetime import datetime
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -11,6 +12,20 @@ app.config.update(dict(
     USERNAME='admin',
     PASSWORD='default'
 ))
+
+class TodoItem:
+    def __init__(self, id, description, status, created_date):
+        self.id = id
+        self.description = description
+        self.status = status
+        self.created_date = created_date
+
+    def to_json(self):
+        return jsonify(
+                id=self.id,
+                description=self.description,
+                status=self.status,
+                created_date=self.created_date)
 
 @app.route('/')
 def index():
@@ -23,9 +38,14 @@ def create():
     entries = cur.fetchall()
     return render_template('show_entries.html', entries=entries)
 
+@app.route('/list', methods=['GET'])
+def list():
+    pass
+
 @app.route('/read', methods=['GET'])
 def read():
-    pass
+    item = TodoItem(117, 'John', False, datetime.now())
+    return item.to_json()
 
 @app.route('/update', methods=['POST'])
 def update():
