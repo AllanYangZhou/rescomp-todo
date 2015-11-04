c4ecaacff3530d8065c75248e0d6fe318d33c9e3
Albert Hu <alberthu@berkeley.edu>
2015-11-03 20:31:21 -0800

    Attempt at delete

--- static/js/script.js
+++ static/js/script.js
@@ -17,11 +17,17 @@ $(function() {
 var reloadList = function() {
 };
 
-$.delete = function(url, data, callback, type) {
-   return  $.ajax({
-        url: url,
-        type: "DELETE",
-        data: data,
-        contentType: type
+$(function() {
+    $("a").on("click", function() {
+        var id = $(this).parent().attr("id")
+        $.ajax({
+            url: "/delete",
+            type: "DELETE",
+            data: JSON.stringify({"id": id}),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function() { console.log("success"); },
+            failure: function() { console.log("failure"); }
+        });
     });
-}
+});
