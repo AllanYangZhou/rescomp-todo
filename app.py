6fa234ad488d9be7459b51802fe798cd917ca67a
samyhajal <hajalsamy@gmail.com>
2015-11-03 18:22:07 -0800

    initial changes to app

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3, os
+
 app = Flask(__name__)
+
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'todo.db'),
+    DEBUG=True,
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/create", methods=['POST'])
+def create():
+    db = get_db()
+    cur = db.execute('INSERT INTO ')
+    entries = cur.fetchall()
+    return render_template('show_entries.html', entries=entries)
+
+
 if __name__ == "__main__":
+    if (!os.path.exists('/this/is/a/dir'))
+    con = sqlite3.connect('sqlite.db')
+    with open('dump.sql', 'w') as f:
+        for line in con.iterdump():
+            f.write('%s\n' % line)
     app.run(debug=True)
