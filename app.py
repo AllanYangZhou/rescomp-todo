62d8a20a099396b2b60845d583d35228823db472
Zizheng Tai <zizheng.tai@gmail.com>
2015-11-03 19:33:48 -0800

    implemented list

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, json, request, Response
+from flask import Flask, render_template, request, jsonify, Response
 import os
 import sqlite3
 from datetime import datetime
@@ -23,28 +23,30 @@ def index():
 def create():
     conn = sqlite3.connect('todo.db')
     c = conn.cursor()
-    c.execute("INSERT INTO todos ('description', 'status') VALUES (?, ?)",
-                (description, status))
+    c.execute('INSERT INTO todos (description, status) VALUES (?, ?)',
+            (description, status))
     try:
         c.commit()
-    except Exception:
+    except:
         c.close()
         return Response(response="db error", status=500)
     c.close()
     return Response(response="success", status=200)
 
-
 @app.route('/list', methods=['GET'])
 def list():
-    pass
-
-@app.route('/read', methods=['GET'])
-def read():
-    try:
-        id = request.args['id']
-    except:
-        return 'bad arguments'
-    return item.to_json()
+    with sqlite3.connect('todo.db') as conn:
+        try:
+            c = conn.cursor()
+            c.execute('SELECT description, status, created_date FROM todos')
+            items = [{'description': description, 'status': status, 'created_date': created_date}
+                     for description, status, created_date in c.fetchall()]
+            result = jsonify(items=items)
+        except Exception as e:
+            result = Response(response="db error", status=500)
+        finally:
+            c.close()
+    return result
 
 @app.route('/update', methods=['POST'])
 def update():
